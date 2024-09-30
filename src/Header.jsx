ced0e86fe903b1681ffee701b6d46673c37e3799
Nwabueze John <143359560+Johnnwabueze01@users.noreply.github.com>
2024-09-30 12:45:05 +0100

    Add files via upload

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import logo from "./assets/logo.png";
+import image from "./assets/johnny.jpg"
 
 export default function Header() {
     return(
@@ -7,6 +8,7 @@ export default function Header() {
         <img src={logo} className="header-image" />
         <h2 className="header-title">Meme Generator</h2>
         <h4 className="header-project">By John Nwabueze</h4>
+        <img src={image} alt="My_Image" className="my-img" />
         </header>
     )
 }
