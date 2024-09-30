3098cc1110f97d4896b17926c94534539088f8e9
Nwabueze John <143359560+Johnnwabueze01@users.noreply.github.com>
2024-09-30 11:20:00 +0100

    Update Header.jsx

--- src/Header.jsx
+++ src/Header.jsx
@@ -6,7 +6,7 @@ export default function Header() {
       <header className="header">
         <img src={logo} className="header-image" />
         <h2 className="header-title">Meme Generator</h2>
-        <h4 className="header-project">React course-project</h4>
+        <h4 className="header-project">By John Nwabueze</h4>
         </header>
     )
 }
